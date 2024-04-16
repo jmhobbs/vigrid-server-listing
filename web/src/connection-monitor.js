db9e7314842eb3d7852c44faa9f882685f8f9bae
John Hobbs <john@velvetcache.org>
2024-04-16 16:21:47 -0500

    Add timestamp to connection monitor.

--- web/src/connection-monitor.js
+++ web/src/connection-monitor.js
@@ -1,7 +1,48 @@
 const template = document.createElement('template');
 
 template.innerHTML = `
-  <div></div>
+  <style>
+div {
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted black;
+}
+
+div #time {
+  visibility: hidden;
+  width: 120px;
+  background-color: #444;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  font-size: 0.75em;
+
+  position: absolute;
+  z-index: 1;
+  top: -0.75em;
+  left: 107%;
+}
+
+div:hover #time {
+  visibility: visible;
+}
+
+div #time::after {
+  content: " ";
+  position: absolute;
+  top: 50%;
+  right: 100%;
+  margin-top: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent #444 transparent transparent;
+}
+  </style>
+  <div>
+    <span id="status"></span>
+    <span id="time">Hello</span>
+  </div>
 `;
 
 // A UI element to show the connection state of the server monitor.
@@ -14,7 +55,8 @@ export default class ConnectionMonitor extends HTMLElement {
 
     this._shadowRoot = this.attachShadow({ 'mode': 'open' });
     this._shadowRoot.appendChild(template.content.cloneNode(true));
-    this.state = this._shadowRoot.querySelector('div');
+    this.state = this._shadowRoot.querySelector('span#status');
+    this.time = this._shadowRoot.querySelector('span#time');
     this._lastUpdate = 0;
     this._reconnecting = false;
   }
@@ -46,6 +88,8 @@ export default class ConnectionMonitor extends HTMLElement {
   }
 
   render() {
+    this.time.innerText = `Last update at ${new Date(this._lastUpdate).toLocaleTimeString()}`;
+
     if(this._reconnecting) {
       this.state.innerText = '🟡 Reconnecting';
       return;
